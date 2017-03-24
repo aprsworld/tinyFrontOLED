e880867ff4f5aa4bfc373054c778ddc1dca41400
James Jarvis <jj@aprsworld.com>
2017-03-24 21:08:31 +0000

    Removed obsolete boilerplate

--- tinyFrontOLED.c
+++ tinyFrontOLED.c
@@ -1,176 +1,14 @@
-/*********************************************************************
-This is an example for our Monochrome OLEDs based on SSD1306 drivers
-
-  Pick one up today in the adafruit shop!
-  ------> http://www.adafruit.com/category/63_98
-
-This example is for a 128x32|64 size display using SPI or I2C to communicate
-4 or 5 pins are required to interface
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-
-02/18/2013 	Charles-Henri Hallard (http://hallard.me)
-						Modified for compiling and use on Raspberry ArduiPi Board
-						LCD size and connection are now passed as arguments on 
-						the command line (no more #define on compilation needed)
-						ArduiPi project documentation http://hallard.me/arduipi
-
-						
-*********************************************************************/
-
 #include "ArduiPi_SSD1306.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
-#include <getopt.h>
-
-#define PRG_NAME        "ssd1306_demo"
-#define PRG_VERSION     "1.1"
-
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <ifaddrs.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 
 // Instantiate the display
 Adafruit_SSD1306 display;
 
-// Config Option
-struct s_opts
-{
-	int oled;
-	int verbose;
-} ;
-
-// default options values
-s_opts opts = {
-	OLED_ADAFRUIT_SPI_128x32,	// Default oled
-  false										// Not verbose
-};
-
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-/* ======================================================================
-Function: usage
-Purpose : display usage
-Input 	: program name
-Output	: -
-Comments: 
-====================================================================== */
-void usage( char * name)
-{
-	printf("%s\n", name );
-	printf("Usage is: %s --oled type [options]\n", name);
-	printf("  --<o>led type\nOLED type are:\n");
-	for (int i=0; i<OLED_LAST_OLED;i++)
-		printf("  %1d %s\n", i, oled_type_str[i]);
-	
-	printf("Options are:\n");
-	printf("  --<v>erbose  : speak more to user\n");
-	printf("  --<h>elp\n");
-	printf("<?> indicates the equivalent short option.\n");
-	printf("Short options are prefixed by \"-\" instead of by \"--\".\n");
-	printf("Example :\n");
-	printf( "%s -o 1 use a %s OLED\n\n", name, oled_type_str[1]);
-	printf( "%s -o 4 -v use a %s OLED being verbose\n", name, oled_type_str[4]);
-}
-
-
-/* ======================================================================
-Function: parse_args
-Purpose : parse argument passed to the program
-Input 	: -
-Output	: -
-Comments: 
-====================================================================== */
-void parse_args(int argc, char *argv[])
-{
-	static struct option longOptions[] =
-	{
-		{"oled"	  , required_argument,0, 'o'},
-		{"verbose", no_argument,	  	0, 'v'},
-		{"help"		, no_argument, 			0, 'h'},
-		{0, 0, 0, 0}
-	};
-
-	int optionIndex = 0;
-	int c;
-
-	while (1) 
-	{
-		/* no default error messages printed. */
-		opterr = 0;
-
-    c = getopt_long(argc, argv, "vho:", longOptions, &optionIndex);
-
-		if (c < 0)
-			break;
-
-		switch (c) 
-		{
-			case 'v': opts.verbose = true	;	break;
-
-			case 'o':
-				opts.oled = (int) atoi(optarg);
-				
-				if (opts.oled < 0 || opts.oled >= OLED_LAST_OLED )
-				{
-						fprintf(stderr, "--oled %d ignored must be 0 to %d.\n", opts.oled, OLED_LAST_OLED-1);
-						fprintf(stderr, "--oled set to 0 now\n");
-						opts.oled = 0;
-				}
-			break;
-
-			case 'h':
-				usage(argv[0]);
-				exit(EXIT_SUCCESS);
-			break;
-			
-			case '?':
-			default:
-				fprintf(stderr, "Unrecognized option.\n");
-				fprintf(stderr, "Run with '--help'.\n");
-				exit(EXIT_FAILURE);
-		}
-	} /* while */
-
-	if (opts.verbose)
-	{
-		printf("%s v%s\n", PRG_NAME, PRG_VERSION);
-		printf("-- OLED params -- \n");
-		printf("Oled is    : %s\n", oled_type_str[opts.oled]);
-		printf("-- Other Stuff -- \n");
-		printf("verbose is : %s\n", opts.verbose? "yes" : "no");
-		printf("\n");
-	}	
-}
-
-
-/* ======================================================================
-Function: main
-Purpose : Main entry Point
-Input 	: -
-Output	: -
-Comments: 
-====================================================================== */
 int main(int argc, char **argv) {
-	struct ifaddrs *ifaddr, *ifa;
-	int family, s;
-	char host[NI_MAXHOST];	
+	int i;
 	
 	// I2C change parameters to fit to your LCD
 	if ( !display.init(OLED_I2C_RESET,2) ) {
@@ -182,59 +20,80 @@ int main(int argc, char **argv) {
 	// init done
 	display.clearDisplay();   // clears the screen and buffer
 
-	/* find local interface ip addresses */
-	if (getifaddrs(&ifaddr) == -1)  {
-		perror("getifaddrs");
-		exit(EXIT_FAILURE);
-	}
+	if ( 5 == argc ) {
+		fprintf(stderr,"# printing 4 lines with top line inverted\n");
 
-	display.setTextSize(1);
-	display.setTextColor(BLACK, WHITE); // 'inverted' text
-	display.setCursor(0,0);
-//	display.printf("IP Address(es):\n");
-	display.printf("IP Address(es):     \n");
-
-	display.setTextSize(1);
-	display.setTextColor(WHITE);
-
-	for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)  {
-		if (ifa->ifa_addr == NULL)
-			continue;  
-
-		s=getnameinfo(ifa->ifa_addr,sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-
-//		if((strcmp(ifa->ifa_name,"wlan0")==0)&&(ifa->ifa_addr->sa_family==AF_INET)) {
-		if ( ( strcmp(ifa->ifa_name,"wlan0")==0 || strcmp(ifa->ifa_name,"eth0")==0) && ( ifa->ifa_addr->sa_family==AF_INET ) ) {
-			if (s != 0) {
-				printf("getnameinfo() failed: %s\n", gai_strerror(s));
-				exit(EXIT_FAILURE);
+		/* truncate lines to 20 characters so they don't wrap */
+		for ( i=1 ; i<5 ; i++ ) {
+			if ( strlen(argv[i]) > 20 ) {
+				argv[i][20]='\0';
 			}
-//			printf("\tInterface : <%s>\n",ifa->ifa_name );
-//			printf("\t  Address : <%s>\n", host); 
-			display.printf("%5s %s\n",ifa->ifa_name,host);
+		}	
+
+		display.setTextSize(1);
+		display.setTextColor(BLACK, WHITE); // 'inverted' text
+		display.setCursor(0,0);
+
+		display.printf("%s\n",argv[1]);
+	
+		display.setTextColor(WHITE);
+		display.printf("%s\n",argv[2]);
+		display.printf("%s\n",argv[3]);
+		display.printf("%s\n",argv[4]);
+
+		display.display();
+	} else if ( 4 == argc ) {
+		fprintf(stderr,"# printing 3 lines with top line inverted and double size\n");
+		display.setTextSize(2);
+		display.setTextColor(BLACK, WHITE); // 'inverted' text
+		display.setCursor(0,0);
+
+		/* truncate to 9 characters */
+		if ( strlen(argv[1]) > 9 ) {
+			argv[1][9]='\0';
 		}
+		display.printf("%s\n",argv[1]);
+	
+
+		/* truncate lines to 20 characters so they don't wrap */
+		for ( i=2 ; i<4 ; i++ ) {
+			if ( strlen(argv[i]) > 20 ) {
+				argv[i][20]='\0';
+			}
+		}	
+
+		display.setTextSize(1);
+		display.setTextColor(WHITE);
+		display.printf("%s\n",argv[2]);
+		display.printf("%s\n",argv[3]);
+
+		display.display();
+	} else if ( 3 == argc ) {
+		fprintf(stderr,"# printing 2 lines with top line inverted and both linesdouble size\n");
+		display.setTextSize(2);
+		display.setTextColor(BLACK, WHITE); // 'inverted' text
+		display.setCursor(0,0);
+
+		/* truncate to 9 characters */
+		for ( i=1 ; i<2 ; i++ ) {
+			if ( strlen(argv[i]) > 9 ) {
+				argv[i][9]='\0';
+			}
+		}
+		display.printf("%s\n",argv[1]);
+	
+
+		display.setTextColor(WHITE);
+		display.printf("%s\n",argv[2]);
+
+		display.display();
+	} else {
+		fprintf(stderr,"# invalid number of arguements\n");
 	}
-
-
-#if 0
-	// text display tests
-	  display.setTextSize(1);
-	  display.setTextColor(WHITE);
-	  display.setCursor(0,0);
-	  display.print("Hello, world!\n");
-	  display.setTextColor(BLACK, WHITE); // 'inverted' text
-	  display.printf("%f\n", 3.141592);
-	  display.setTextSize(2);
-	  display.setTextColor(WHITE);
-	  display.printf("0x%8X\n", 0xDEADBEEF);
-#endif
-
-	  display.display();
 
 	// Free PI GPIO ports
 	display.close();
 
-	freeifaddrs(ifaddr);
 }
 
 
